c92a0e7959392e6e14b33df6c8f5f044a1831642
Ylt_941227 <cdyangliting>
2018-05-06 22:05:21 +0800

    change date

--- app/schemas/article.js
+++ app/schemas/article.js
@@ -1,19 +1,19 @@
 /**
  * Created by Administrator on 2017/10/31.
  */
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var ObjectId = Schema.Types.ObjectId;
-let month = Math.floor(2*Math.random());
-let day = Math.floor(30*Math.random());
-let hour = Math.floor(8+14*Math.random());
-let minute = Math.floor(60*Math.random());
-let second = Math.floor(60*Math.random());
-let date1 = Date.UTC(2017,11,day,hour,minute,second);
-let date2 = Date.UTC(2018,month,day,hour,minute,second);
-let dateArr = [date1, date2];
-let index = Math.floor(2*Math.random());
-let date = dateArr[index];
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+// var ObjectId = Schema.Types.ObjectId;
+// let month = Math.floor(2*Math.random());
+// let day = Math.floor(30*Math.random());
+// let hour = Math.floor(8+14*Math.random());
+// let minute = Math.floor(60*Math.random());
+// let second = Math.floor(60*Math.random());
+// let date1 = Date.UTC(2017,11,day,hour,minute,second);
+// let date2 = Date.UTC(2018,month,day,hour,minute,second);
+// let dateArr = [date1, date2];
+// let index = Math.floor(2*Math.random());
+// let date = dateArr[index];
 var ArticleSchema = Schema({
      title:{
          type:String,
@@ -46,41 +46,41 @@ var ArticleSchema = Schema({
      meta:{
          createAt:{
              type:Date,
-             // default:Date.now()
+             default:Date.now()
              default: date
          },
          updateAt:{
              type:Date,
-             // default:Date.now()
+             default:Date.now()
              default: date
          }
      }
 });
 
 ArticleSchema.pre('save',function (next) {
-    let month = Math.floor(2*Math.random());
-    let day = Math.floor(30*Math.random());
-    let hour = Math.floor(8+14*Math.random());
-    let minute = Math.floor(60*Math.random());
-    let second = Math.floor(60*Math.random());
-    let date1 = Date.UTC(2017,11,day,hour,minute,second);
-    let date2 = Date.UTC(2018,month,day,hour,minute,second);
-    let dateArr = [date1, date2];
-    let index = Math.floor(2*Math.random());
-    let date = dateArr[index];
+    // let month = Math.floor(2*Math.random());
+    // let day = Math.floor(30*Math.random());
+    // let hour = Math.floor(8+14*Math.random());
+    // let minute = Math.floor(60*Math.random());
+    // let second = Math.floor(60*Math.random());
+    // let date1 = Date.UTC(2017,11,day,hour,minute,second);
+    // let date2 = Date.UTC(2018,month,day,hour,minute,second);
+    // let dateArr = [date1, date2];
+    // let index = Math.floor(2*Math.random());
+    // let date = dateArr[index];
     if(this.isNew) {
-        // this.meta.createAt = this.meta.updateAt = Date.now();
-        this.meta.createAt = this.meta.updateAt =  date;
+        this.meta.createAt = this.meta.updateAt = Date.now();
+       // this.meta.createAt = this.meta.updateAt =  date;
     } else {
-        // this.meta.updateAt = Date.now();
-        let tempDate = this.meta.updateAt;
-        tempDate.setDate(tempDate.getDate()+1);
-        this.meta.updateAt =  tempDate;
+         this.meta.updateAt = Date.now();
+        // let tempDate = this.meta.updateAt;
+        // tempDate.setDate(tempDate.getDate()+1);
+        // this.meta.updateAt =  tempDate;
     }
-    console.log('this.meta.createAt');
-    console.log(this.meta.createAt);
-    console.log('this.meta.updateAt');
-    console.log(this.meta.updateAt);
+    // console.log('this.meta.createAt');
+    // console.log(this.meta.createAt);
+    // console.log('this.meta.updateAt');
+    // console.log(this.meta.updateAt);
    next();
 });
 
